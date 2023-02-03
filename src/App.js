05e14dd593132d26c01cb3f57543394db04deda7
Armando <armandocrescencio343@gmail.com>
2023-02-03 01:43:11 -0600

    ADD Detail Component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import Header from './components/Header'
 import Home from './components/Home';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Detail from './components/Detail'
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,  //'Switch' replaced to 'Routes' in react-router-dom v6
   Route,
   Link
 } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App"> 
-      <Header/>  
-      <Home/>
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="detail" element={<Detail/>}/>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </Router>
     </div>
-  );
+  );  
 }
 
 export default App;
